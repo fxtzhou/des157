1b4a5ec94ddeaa67112cfb94fa2c9bfa8cd3638a
Fiona Zhou <fxzhou@ucdavis.edu>
2018-02-22 00:00:18 -0800

    Uploaded studio4
    Studio4 uploaded
    changed index on portal page to reflect

---
+++ studios/studio4/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var answer1 = document.getElementById('answer1');
+var answer2 = document.getElementById('answer2');
+var answer3 = document.getElementById('answer3');
+var answer4 = document.getElementById('answer4');
+var overlay = document.getElementById('overlay');
+var close = document.getElementById('close');
+
+
+answer1.addEventListener('click',function(){
+    overlay.style.display = "block";
+  })
+
+  answer2.addEventListener('click',function(){
+      overlay.style.display = "block";
+    })
+
+    answer3.addEventListener('click',function(){
+        overlay.style.display = "block";
+      })
+
+      answer4.addEventListener('click',function(){
+          overlay.style.display = "block";
+        })
